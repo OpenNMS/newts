4325896a39c5cc0a12c4ddccf2aff3d09673d54d
Eric Evans <eevans@sym-link.com>
2013-12-28 18:10:22 -0600

    add string attributes to measurements

--- api/src/main/java/org/opennms/newts/api/Measurement.java
+++ api/src/main/java/org/opennms/newts/api/Measurement.java
@@ -1,5 +1,9 @@
 package org.opennms.newts.api;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Map;
+
 
 public class Measurement {
 
@@ -7,20 +11,20 @@ public class Measurement {
     private final String m_resource;
     private final String m_name;
     private final MetricType m_type;
-    private final String m_units;
     private final double m_value;
+    private final Map<String, String> m_attributes;
 
     public Measurement(Timestamp timestamp, String resource, String name, MetricType type, double value) {
-        this(timestamp, resource, name, type, null, value);
+        this(timestamp, resource, name, type, value, null);
     }
 
-    public Measurement(Timestamp timestamp, String resource, String name, MetricType type, String units, double value) {
-        m_timestamp = timestamp;
-        m_resource = resource;
-        m_name = name;
-        m_type = type;
-        m_units = units;
-        m_value = value;
+    public Measurement(Timestamp timestamp, String resource, String name, MetricType type, double value, Map<String, String> attributes) {
+        m_timestamp = checkNotNull(timestamp, "timestamp");
+        m_resource = checkNotNull(resource, "resource");
+        m_name = checkNotNull(name, "name");
+        m_type = checkNotNull(type, "type");
+        m_value = checkNotNull(value, "value");
+        m_attributes = attributes;
     }
 
     public Timestamp getTimestamp() {
@@ -39,12 +43,12 @@ public class Measurement {
         return m_type;
     }
 
-    public String getUnits() {
-        return m_units;
-    }
-
     public double getValue() {
         return m_value;
+    }
+
+    Map<String, String> getAttributes() {
+        return m_attributes;
     }
 
 }
