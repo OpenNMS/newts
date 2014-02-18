1bc6309256350a92413dbf08296c82a0bb2f187e
Eric Evans <eevans@sym-link.com>
2014-02-18 11:15:13 -0600

    duration comparison

--- api/src/main/java/org/opennms/newts/api/Duration.java
+++ api/src/main/java/org/opennms/newts/api/Duration.java
@@ -4,7 +4,7 @@ package org.opennms.newts.api;
 import java.util.concurrent.TimeUnit;
 
 
-public class Duration {
+public class Duration implements Comparable<Duration> {
 
     private final long m_duration;
     private final TimeUnit m_unit;
@@ -46,9 +46,37 @@ public class Duration {
         return new Duration(getDuration() * value, getUnit());
     }
 
+    @Override
     public String toString() {
         return String.format("%s[%d, %s]", getClass().getSimpleName(), m_duration, getUnit());
     }
 
-}
+    @Override
+    public int compareTo(Duration o) {
+        TimeUnit unit = Timestamp.finest(getUnit(), o.getUnit());
+        return convert(unit) < o.convert(unit) ? -1 : (convert(unit) > o.convert(unit) ? 1 : 0);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Duration)) return false;
+        return compareTo((Duration) o) == 0;
+    }
+
+    public boolean lt(Duration o) {
+        return compareTo(o) == -1;
+    }
+
+    public boolean gt(Duration o) {
+        return compareTo(o) == 1;
+    }
+
+    public boolean lte(Duration o) {
+        return compareTo(o) <= 0;
+    }
+
+    public boolean gte(Duration o) {
+        return compareTo(o) >= 0;
+    }
+
+}
--- api/src/test/java/org/opennms/newts/api/TimestampDurationTest.java
+++ api/src/test/java/org/opennms/newts/api/TimestampDurationTest.java
@@ -1,10 +1,12 @@
 package org.opennms.newts.api;
 
+
 import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
+
 
 public class TimestampDurationTest {
 
@@ -14,37 +16,45 @@ public class TimestampDurationTest {
         assertFalse(Timestamp.isFiner(TimeUnit.SECONDS, TimeUnit.MILLISECONDS));
         assertFalse(Timestamp.isFiner(TimeUnit.MILLISECONDS, TimeUnit.MILLISECONDS));
         assertFalse(Timestamp.isFiner(TimeUnit.SECONDS, TimeUnit.SECONDS));
-        
-        
+
         Duration sec = Duration.seconds(1);
         Duration kMillis = Duration.millis(1000);
         Duration milli = Duration.millis(1);
-        
+
         Timestamp before = new Timestamp(1, TimeUnit.SECONDS);
-        
+
         Timestamp beforeMillis = new Timestamp(1000, TimeUnit.MILLISECONDS);
-        
+
         Timestamp afterASec = before.plus(sec);
-        
+
         assertEquals(afterASec.m_time, 2);
         assertEquals(afterASec.m_unit, TimeUnit.SECONDS);
-        
+
         Timestamp after1kMillis = before.plus(kMillis);
-        
+
         assertEquals(after1kMillis.m_time, 2000);
         assertEquals(after1kMillis.m_unit, TimeUnit.MILLISECONDS);
-        
+
         Timestamp twoKMillis = beforeMillis.plus(sec);
-        
+
         assertEquals(twoKMillis.m_time, 2000);
         assertEquals(twoKMillis.m_unit, TimeUnit.MILLISECONDS);
-        
+
         Timestamp after1Milli = before.plus(milli);
-        
+
         assertEquals(after1Milli.m_time, 1001);
         assertEquals(after1Milli.m_unit, TimeUnit.MILLISECONDS);
-        
-        
+
+        // Duration comparison
+        assertTrue(sec.equals(Duration.millis(1000)));
+        assertTrue(Duration.millis(1000).equals(Duration.seconds(1)));
+        assertTrue(sec.gt(milli));
+        assertTrue(sec.gte(milli));
+        assertTrue(milli.lt(sec));
+        assertTrue(milli.lte(sec));
+        assertTrue(milli.lte(Duration.millis(1)));
+        assertTrue(milli.gte(Duration.millis(1)));
+
     }
 
 }
