01fcfb36fb381407bd8fa773a1fa7ffc3f7201d4
Eric Evans <eevans@sym-link.com>
2014-03-06 10:05:30 -0600

    helper classes for building test measurements/samples

--- cassandra/src/test/java/org/opennms/newts/persistence/cassandra/Utils.java
+++ cassandra/src/test/java/org/opennms/newts/persistence/cassandra/Utils.java
@@ -15,12 +15,92 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import org.opennms.newts.api.Element;
 import org.opennms.newts.api.Measurement;
+import org.opennms.newts.api.MetricType;
+import org.opennms.newts.api.Results;
 import org.opennms.newts.api.Results.Row;
+import org.opennms.newts.api.Sample;
+import org.opennms.newts.api.Timestamp;
+import org.opennms.newts.api.ValueType;
 import org.opennms.newts.api.query.ResultDescriptor;
 
 
 class Utils {
+
+    static abstract class AbstractRowsBuilder<T extends Element<?>> {
+
+        private final Results<T> m_results;
+        private final String m_resource;
+
+        private Row<T> m_current;
+
+        AbstractRowsBuilder(String resource) {
+            m_resource = checkNotNull(resource, "resource argument");
+            m_results = new Results<>();
+        }
+
+        AbstractRowsBuilder<T> row(Timestamp timestamp) {
+            m_current = new Row<>(timestamp, m_resource);
+            m_results.addRow(m_current);
+            return this;
+        }
+
+        AbstractRowsBuilder<T> row(int epochSeconds) {
+            return row(Timestamp.fromEpochSeconds(epochSeconds));
+        }
+
+        protected String getResource() {
+            return m_resource;
+        }
+
+        protected Timestamp getCurrentTimestamp() {
+            return m_current.getTimestamp();
+        }
+
+        protected void addElement(T element) {
+            m_current.addElement(element);
+        }
+
+        abstract AbstractRowsBuilder<T> element(String name, double value);
+
+        Results<T> build() {
+            return m_results;
+        }
+
+    }
+
+    static class MeasurementRowsBuilder extends AbstractRowsBuilder<Measurement> {
+
+        MeasurementRowsBuilder(String resource) {
+            super(resource);
+        }
+
+        @Override
+        MeasurementRowsBuilder element(String name, double value) {
+            addElement(new Measurement(getCurrentTimestamp(), getResource(), name, value));
+            return this;
+        }
+
+    }
+
+    static class SampleRowsBuilder extends AbstractRowsBuilder<Sample> {
+
+        private final MetricType m_type;
+
+        SampleRowsBuilder(String resource, MetricType type) {
+            super(resource);
+
+            m_type = checkNotNull(type, "type argument");
+        }
+
+        @Override
+        SampleRowsBuilder element(String name, double value) {
+            addElement(new Sample(getCurrentTimestamp(), getResource(), name, m_type, ValueType.compose(value, m_type)));
+            return this;
+        }
+
+    }
 
     private static Unmarshaller s_unmarshaller;
 
