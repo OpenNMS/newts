d3a3bd0c54318a04d4eb17b2351744077cbfdc2d
Eric Evans <eevans@sym-link.com>
2014-03-06 11:16:20 -0600

    rewrite ComputeTest to use builder

--- cassandra/src/test/java/org/opennms/newts/persistence/cassandra/ComputeTest.java
+++ cassandra/src/test/java/org/opennms/newts/persistence/cassandra/ComputeTest.java
@@ -1,69 +1,52 @@
 package org.opennms.newts.persistence.cassandra;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.opennms.newts.api.Timestamp.fromEpochSeconds;
 import static org.opennms.newts.api.query.StandardAggregationFunctions.AVERAGE;
+import static org.opennms.newts.persistence.cassandra.Utils.assertRowsEqual;
 
 import org.junit.Test;
 import org.opennms.newts.api.Measurement;
 import org.opennms.newts.api.Results;
-import org.opennms.newts.api.Results.Row;
 import org.opennms.newts.api.query.ResultDescriptor;
 import org.opennms.newts.api.query.ResultDescriptor.BinaryFunction;
+import org.opennms.newts.persistence.cassandra.Utils.MeasurementRowsBuilder;
 
 
 public class ComputeTest {
 
-    @Test
-    public void test() {
+    private static final BinaryFunction ADD;
 
-        Results<Measurement> testData = new Results<>();
-
-        testData.addElement(new Measurement(fromEpochSeconds(300), "localhost", "in", 2));
-        testData.addElement(new Measurement(fromEpochSeconds(300), "localhost", "out", 2));
-
-        testData.addElement(new Measurement(fromEpochSeconds(600), "localhost", "in", 6));
-        testData.addElement(new Measurement(fromEpochSeconds(600), "localhost", "out", 4));
-
-        ResultDescriptor rDescriptor = new ResultDescriptor().datasource("in", AVERAGE).datasource("out", AVERAGE);
-
-        BinaryFunction func = new BinaryFunction() {
+    static {
+        ADD = new BinaryFunction() {
 
             @Override
             public double apply(double a, double b) {
                 return a + b;
             }
         };
+    }
 
-        rDescriptor.calculate("total", func, "in", "out");
+    @Test
+    public void test() {
+
+        Results<Measurement> testData = new MeasurementRowsBuilder("localhost")
+                .row(300).element("in", 2).element("out", 2)
+                .row(600).element("in", 6).element("out", 4)
+                .build();
+
+        ResultDescriptor rDescriptor = new ResultDescriptor()
+                .datasource("in",  AVERAGE)
+                .datasource("out", AVERAGE)
+                .calculate("total", ADD, "in", "out");
+
+        Results<Measurement> expected = new MeasurementRowsBuilder("localhost")
+                .row(300).element("in", 2).element("out", 2).element("total", 4)
+                .row(600).element("in", 6).element("out", 4).element("total", 10)
+                .build();
 
         Compute compute = new Compute(rDescriptor, testData.iterator());
 
-        assertTrue("Insufficient results", compute.hasNext());
-
-        Row<Measurement> row = compute.next();
-        assertNotNull("Missing element \"total\"", row.getElement("total"));
-        assertNotNull("Missing element \"in\"", row.getElement("in"));
-        assertNotNull("Missing element \"out\"", row.getElement("out"));
-        assertEquals(4, row.getElement("total").getValue(), 0.0d);
-        assertEquals(2, row.getElement("in").getValue(), 0.0d);
-        assertEquals(2, row.getElement("out").getValue(), 0.0d);
-
-        assertTrue("Insufficient results", compute.hasNext());
-
-        row = compute.next();
-        assertNotNull("Missing element \"total\"", row.getElement("total"));
-        assertNotNull("Missing element \"in\"", row.getElement("in"));
-        assertNotNull("Missing element \"out\"", row.getElement("out"));
-        assertEquals(10, row.getElement("total").getValue(), 0.0d);
-        assertEquals(6, row.getElement("in").getValue(), 0.0d);
-        assertEquals(4, row.getElement("out").getValue(), 0.0d);
-
-        assertFalse("Too many results", compute.hasNext());
+        assertRowsEqual(expected, compute);
 
     }
 
