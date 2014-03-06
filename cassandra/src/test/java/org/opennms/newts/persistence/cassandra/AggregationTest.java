2b91c2ee7a6f85f1596658b8df928b88fd841f68
Eric Evans <eevans@sym-link.com>
2014-03-06 11:05:46 -0600

    rewrite AggregationTest to use builders instead of xml

--- cassandra/src/test/java/org/opennms/newts/persistence/cassandra/AggregationTest.java
+++ cassandra/src/test/java/org/opennms/newts/persistence/cassandra/AggregationTest.java
@@ -1,31 +1,61 @@
 package org.opennms.newts.persistence.cassandra;
 
 
-import static org.opennms.newts.persistence.cassandra.Utils.getResultDescriptor;
+import static java.lang.Double.NaN;
+import static org.opennms.newts.api.query.StandardAggregationFunctions.AVERAGE;
+import static org.opennms.newts.api.query.StandardAggregationFunctions.MAX;
+import static org.opennms.newts.api.query.StandardAggregationFunctions.MIN;
+import static org.opennms.newts.persistence.cassandra.Utils.assertRowsEqual;
 
 import org.junit.Test;
+import org.opennms.newts.api.Duration;
+import org.opennms.newts.api.Measurement;
+import org.opennms.newts.api.Results;
+import org.opennms.newts.api.Timestamp;
+import org.opennms.newts.api.query.ResultDescriptor;
+import org.opennms.newts.persistence.cassandra.Utils.MeasurementRowsBuilder;
 
 
-public class AggregationTest extends AbstractXMLTestCase {
-
-    Aggregation getIterator(XMLTestSpecification testCase) {
-        return new Aggregation(
-                testCase.getResource(),
-                testCase.getStart(),
-                testCase.getEnd(),
-                getResultDescriptor(testCase),
-                testCase.getResolution(),
-                testCase.getTestDataAsMeasurements().iterator());
-    }
+public class AggregationTest {
 
     @Test
-    public void testMultipleAggregations() {
-        execute("aggregation/multiple_aggregations.xml");
-    }
+    public void test() {
 
-    @Test
-    public void testOneHour() {
-        execute("aggregation/one_hour.xml");
+        Results<Measurement> testData = new MeasurementRowsBuilder("localhost")
+                .row(   1).element("m0", 1)
+                .row( 300).element("m0", 1)
+                .row( 600).element("m0", 1)
+                .row( 900).element("m0", 1)
+                .row(1200).element("m0", 1)
+                .row(1500).element("m0", 1)
+                .row(1800).element("m0", 3)
+                .row(2100).element("m0", 3)
+                .row(2400).element("m0", 3)
+                .row(2700).element("m0", 3)
+                .row(3000).element("m0", 3)
+                .row(3300).element("m0", 3)
+                .build();
+
+        ResultDescriptor rDescriptor = new ResultDescriptor(Duration.seconds(300))
+                .datasource("m0-avg", "m0", Duration.seconds(600), AVERAGE)
+                .datasource("m0-min", "m0", Duration.seconds(600), MIN)
+                .datasource("m0-max", "m0", Duration.seconds(600), MAX);
+
+        Results<Measurement> expected = new MeasurementRowsBuilder("localhost")
+                .row(   0).element("m0-avg", NaN).element("m0-min", NaN).element("m0-max", NaN)
+                .row(3600).element("m0-avg",   2).element("m0-min",   1).element("m0-max",   3)
+                .build();
+
+        Aggregation aggregation = new Aggregation(
+                "localhost",
+                Timestamp.fromEpochSeconds(   1),
+                Timestamp.fromEpochSeconds(3300),
+                rDescriptor,
+                Duration.minutes(60),
+                testData.iterator());
+
+        assertRowsEqual(expected, aggregation);
+
     }
 
 }
