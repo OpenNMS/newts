9970b2567c4d110d2985cb87b36349a9d42aa135
Eric Evans <eevans@sym-link.com>
2014-03-06 11:27:21 -0600

    rewrite ExportTest to use builder instead of xml

--- cassandra/src/test/java/org/opennms/newts/persistence/cassandra/ExportTest.java
+++ cassandra/src/test/java/org/opennms/newts/persistence/cassandra/ExportTest.java
@@ -1,28 +1,38 @@
 package org.opennms.newts.persistence.cassandra;
 
 
-import static org.opennms.newts.persistence.cassandra.Utils.getResultDescriptor;
+import static org.opennms.newts.persistence.cassandra.Utils.assertRowsEqual;
 
 import org.junit.Test;
 import org.opennms.newts.api.Measurement;
-import org.opennms.newts.api.Results.Row;
+import org.opennms.newts.api.Results;
+import org.opennms.newts.api.query.ResultDescriptor;
+import org.opennms.newts.persistence.cassandra.Utils.MeasurementRowsBuilder;
 
-
-public class ExportTest extends AbstractXMLTestCase {
-
-    @Test
-    public void testSubset() {
-        execute("export/subset.xml");
-    }
+public class ExportTest {
 
     @Test
-    public void testExportAll() {
-        execute("export/all.xml");
-    }
+    public void test() {
 
-    @Override
-    Iterable<Row<Measurement>> getIterator(XMLTestSpecification testSpec) {
-        return new Export(getResultDescriptor(testSpec).getExports(), testSpec.getTestDataAsMeasurements().iterator());
+        // Rows with measurements for "m0", "m1", and "m2".
+        Results<Measurement> testData = new MeasurementRowsBuilder("localhost")
+                .row(  1).element("m0", 1).element("m1", 2).element("m2", 3)
+                .row(300).element("m0", 1).element("m1", 2).element("m2", 3)
+                .row(600).element("m0", 1).element("m1", 2).element("m2", 3)
+                .build();
+
+        // ResultDescriptor that exports only "m1".
+        ResultDescriptor rDescriptor = new ResultDescriptor().datasource("m1", null).export("m1");
+
+        // Expected results.
+        Results<Measurement> expected = new MeasurementRowsBuilder("localhost")
+                .row(  1).element("m1", 2)
+                .row(300).element("m1", 2)
+                .row(600).element("m1", 2)
+                .build();
+
+        assertRowsEqual(expected, new Export(rDescriptor.getExports(), testData.iterator()));
+
     }
 
 }
