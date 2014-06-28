2558598f8c3bd6d8ade8b85beb93921092de16b1
Eric Evans <eevans@sym-link.com>
2014-06-27 21:02:15 -0500

    report insert rate on test completion

--- examples/stress/src/main/java/org/opennms/newts/stress/Dispatcher.java
+++ examples/stress/src/main/java/org/opennms/newts/stress/Dispatcher.java
@@ -8,6 +8,8 @@ import java.util.concurrent.BlockingQueue;
 
 import org.opennms.newts.api.Sample;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.Queues;
 
 
@@ -16,6 +18,7 @@ abstract class Dispatcher {
     protected final Config m_config;
     protected final BlockingQueue<Collection<Sample>> m_samplesQueue;
     protected final Worker[] m_threads;
+    protected final MetricRegistry m_metricRegistry = new MetricRegistry();
 
     Dispatcher(Config config) {
         m_config = checkNotNull(config, "config argument");
@@ -35,6 +38,10 @@ abstract class Dispatcher {
             w.join();
         }
 
+    }
+
+    void printReport() {
+        ConsoleReporter.forRegistry(m_metricRegistry).build().report();
     }
 
 }
